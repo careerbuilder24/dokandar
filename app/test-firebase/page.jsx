cc7eac8986b72fdc9e0ead9f32a0eb902da82696
rudrahasan1111-hub <rudrahasan1111@gmail.com>
2026-02-05 17:05:20 +0600

    fix

--- app/test-firebase/page.jsx
+++ app/test-firebase/page.jsx
@@ -1,7 +1,27 @@
+// "use client";
+
+// import { useEffect } from "react";
+// import { auth, db } from "../lib/firebase";
+
+// export default function TestFirebase() {
+//   useEffect(() => {
+//     console.log("Firebase Auth:", auth);
+//     console.log("Firestore DB:", db);
+//   }, []);
+
+//   return (
+//     <div className="p-10">
+//       <h1 className="text-xl font-bold">
+//         Firebase Connected ✅
+//       </h1>
+//       <p>Check console for Firebase objects</p>
+//     </div>
+//   );
+// }
 "use client";
 
 import { useEffect } from "react";
-import { auth, db } from "../lib/firebase";
+import { auth, db } from "@/app/lib/firebase";
 
 export default function TestFirebase() {
   useEffect(() => {
