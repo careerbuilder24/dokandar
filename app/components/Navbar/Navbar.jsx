ca489920f37e09246475ea6d518e48911f123a2a
rudrahasan1111-hub <rudrahasan1111@gmail.com>
2026-02-05 18:50:34 +0600

    ff

--- app/components/Navbar/Navbar.jsx
+++ app/components/Navbar/Navbar.jsx
@@ -245,7 +245,7 @@ export default function Navbar() {
 
               <Link href="/" className="flex items-center gap-2">
                 <Image
-                  src="/"
+                  src="/logo2.png"
                   alt="Dokandar Logo"
                   width={40}
                   height={40}
